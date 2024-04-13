8d0fe6afd4997f87a306163f6d834e69713d172b
Maxim <159849110+mxnix@users.noreply.github.com>
2024-04-13 21:50:56 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,8 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Camera
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
+LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Camera.apk
+LOCAL_OVERRIDES_PACKAGES := Aperture SnapdragonCamera SnapdragonCamera2 Snap Camera2
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.camera.extensions.impl
 
 include $(BUILD_PREBUILT)
